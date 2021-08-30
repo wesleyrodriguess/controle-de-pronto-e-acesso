7cadeeeb319253dcd550243ebafecd2a1e10d92d
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2021-08-30 13:01:11 -0300

    feat:add getAll and getById jornadaTrabalho

--- src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
+++ src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
@@ -3,10 +3,11 @@ package com.dio.live.controller;
 import com.dio.live.model.JornadaTrabalho;
 import com.dio.live.service.JornadaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/jornada-trabalho")
@@ -18,5 +19,16 @@ public class JornadaTrabalhoController {
     @PostMapping
     public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
         return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> getJornadaList(){
+        return jornadaService.findAll();
+    }
+
+    @GetMapping("/{idJornada}")
+    public ResponseEntity<JornadaTrabalho> getJornadaByID(@PathVariable("idJornada") Long idJornada) throws Exception {
+        return  ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Not found!")));
+
     }
 }
--- src/main/java/com/dio/live/service/JornadaService.java
+++ src/main/java/com/dio/live/service/JornadaService.java
@@ -5,6 +5,9 @@ import com.dio.live.repository.JornadaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class JornadaService {
 
@@ -17,5 +20,13 @@ public class JornadaService {
 
     public JornadaTrabalho saveJornada(JornadaTrabalho jornadaTrabalho){
         return jornadaRepository.save(jornadaTrabalho);
+    }
+
+    public List<JornadaTrabalho> findAll() {
+        return   jornadaRepository.findAll();
+    }
+
+    public Optional<JornadaTrabalho> getById(Long idJornada) {
+        return jornadaRepository.findById(idJornada);
     }
 }
