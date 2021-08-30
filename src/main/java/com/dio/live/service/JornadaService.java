804f34695df5fcc5f8812d5f800a264cff7a2a24
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2021-08-30 13:47:00 -0300

    fead:add delete jornadaTrabalho service and add SwaggerConfig

--- src/main/java/com/dio/live/service/JornadaService.java
+++ src/main/java/com/dio/live/service/JornadaService.java
@@ -4,6 +4,7 @@ import com.dio.live.model.JornadaTrabalho;
 import com.dio.live.repository.JornadaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,19 +19,28 @@ public class JornadaService {
         this.jornadaRepository = jornadaRepository;
     }
 
+    @Transactional
     public JornadaTrabalho saveJornada(JornadaTrabalho jornadaTrabalho){
         return jornadaRepository.save(jornadaTrabalho);
     }
 
+    @Transactional
+    public JornadaTrabalho updateJornada(JornadaTrabalho jornadaTrabalho){
+        return jornadaRepository.save(jornadaTrabalho);
+    }
+
+    @Transactional
+    public void deleteJornada(Long idJornada) {
+        jornadaRepository.deleteById(idJornada);
+    }
+
+    @Transactional(readOnly = true)
     public List<JornadaTrabalho> findAll() {
         return   jornadaRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Optional<JornadaTrabalho> getById(Long idJornada) {
         return jornadaRepository.findById(idJornada);
-    }
-
-    public JornadaTrabalho updateJornada(JornadaTrabalho jornadaTrabalho){
-        return jornadaRepository.save(jornadaTrabalho);
     }
 }
